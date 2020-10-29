aac652711dd296aa17236b30f7e54a652d648a2b
raj1020 <raj.1020@hotmail.com>
2020-10-29 15:24:35 +1100

    defined the input fields for the registration form

--- src/components/Auth/Register/Register.component.jsx
+++ src/components/Auth/Register/Register.component.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React from 'react';
+import { Form, Grid, Header, Icon, Segment } from 'semantic-ui-react';
+
 
 function Register() {
+
+    const handleInput = (event) => {
+
+    }
+
+
     return (
-        <div>
-            Register
-        </div>
+        <Grid>
+            <Grid.Column style={{maxWidth: '500px'}}>
+            <Header icon as="h2">
+                <Icon name="slack" />
+                Register
+            </Header>
+                <Form>
+                    <Segment stacked>
+                        <Form.Input
+                            name="userName"
+                            value=""
+                            icon="user"
+                            iconPosition="left"
+                            onChange={handleInput}
+                            type="text"
+                            placeholder="User Name"
+
+                        />
+                        <Form.Input
+                            name="email"
+                            value=""
+                            icon="mail"
+                            iconPosition="left"
+                            onChange={handleInput}
+                            type="text"
+                            placeholder="User Email"
+
+                        />
+                        <Form.Input
+                            name="password"
+                            value=""
+                            icon="lock"
+                            iconPosition="left"
+                            onChange={handleInput}
+                            type="password"
+                            placeholder="User Password"
+
+                        />
+                        <Form.Input
+                            name="confirmpassword"
+                            value=""
+                            icon="lock"
+                            iconPosition="left"
+                            onChange={handleInput}
+                            type="password"
+                            placeholder="Confirm Password"
+
+                        />
+                    </Segment>
+                </Form>
+            </Grid.Column>
+        </Grid>
     )
 }
 
